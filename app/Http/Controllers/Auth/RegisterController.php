44b1043cd78725cd81c7e7d7497cd61e6952646d
Andrew Li <andrew.li1987@yandex.com>
2017-06-14 02:06:08 -0700

    mail function

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -62,10 +62,56 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+        sendEmail($data);
+
+
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
         ]);
+    }
+
+    public function sendEmail(array $data){
+        $to = $data['email']; // note the comma
+
+        // Subject
+        $subject = 'Welcome to Talent Team';
+
+        // Message
+        $message = '
+        <html>
+        <head>
+          <title>Welcome to Talent Team</title>
+        </head>
+        <body>
+          <p>Before joining in our Team, please verify your email!</p>
+          <table>
+            <tr>
+              <th>Person</th><th>Day</th><th>Month</th><th>Year</th>
+            </tr>
+            <tr>
+              <td>Johny</td><td>10th</td><td>August</td><td>1970</td>
+            </tr>
+            <tr>
+              <td>Sally</td><td>17th</td><td>August</td><td>1973</td>
+            </tr>
+          </table>
+        </body>
+        </html>
+        ';
+
+        // To send HTML mail, the Content-type header must be set
+        $headers[] = 'MIME-Version: 1.0';
+        $headers[] = 'Content-type: text/html; charset=iso-8859-1';
+
+        // Additional headers
+        $headers[] = 'To: '. $to;
+        $headers[] = 'From: Team Service <service@probateproject.com>';
+        //$headers[] = 'Cc: birthdayarchive@example.com';
+       // $headers[] = 'Bcc: birthdaycheck@example.com';
+
+        // Mail it
+        mail($to, $subject, $message, implode("\r\n", $headers));
     }
 }
--- config/mail.php
+++ config/mail.php
@@ -29,7 +29,7 @@ return [
     |
     */
 
-    'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+    'host' => env('MAIL_HOST', 'smtp.probateproject.com'),
 
     /*
     |--------------------------------------------------------------------------
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'service@probateproject.com'),
+        'name' => env('MAIL_FROM_NAME', 'Service'),
     ],
 
     /*
